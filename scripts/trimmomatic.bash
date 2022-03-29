85440d6dadd4199361050c8b477de65d16d3406d
Emma Murari <emmurari@asu.edu>
2022-03-29 14:22:15 -0700

    Finish trimmomatic script

--- scripts/trimmomatic.bash
+++ scripts/trimmomatic.bash
@@ -7,19 +7,28 @@
 
 
 #Load module
+module purge
 module add trimmomatic/0.33
 
-TRIM_BIN=java -jar /packages/7x/trimmomatic/0.33/trimmomatic.jar "$@"
+infile="$1"
+outdir="$2"
 
+mkdir -p "$outdir"
+
+#Trim Jar
+TRIM_BIN=java -jar /packages/7x/trimmomatic/0.33/trimmomatic.jar 
+TRIM_ARGS="SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:scripts/NexteraPE-PE.fa:2:40:15" 
 
 
 #This is a for loop that will allow the user to trim multiple sets of paired end reads
-#This requires the use of the trimmomatic wrapper trimmomatic.bash
 
-base=$(basename ${base} _1.fastq.gz)
+#Create basename
+base=$(basename "${infile}" _1.fastq.gz)
+indir=$(dirname "${infile}")
 
-TRIM_BIN PE ${base}_1.fastq.gz ${base}_2.fastq.gz \
-                ${base}_1.trim.fastq.gz ${base}_1un.trim.fastq.gz \
-                ${base}_2.trim.fastq.gz ${base}_2un.trim.fastq.gz \
-                SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15 
- done
+#Run Trimmomatic
+$TRIM_BIN PE "${indir}/${base}_1.fastq.gz" "${indir}/${base}_2.fastq.gz" \
+                "${indir}/${base}_1.trim.fastq.gz" "${indir}/${base}_1un.trim.fastq.gz" \
+                "${indir}/${base}_2.trim.fastq.gz" "${indir}/${base}_2un.trim.fastq.gz" \
+		${TRIM_ARGS}
+ 
