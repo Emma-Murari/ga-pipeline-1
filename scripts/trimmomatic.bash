b25c01faee650fb19ca28101ec2975c2c94d8998
Emma-Murari <101903200+Emma-Murari@users.noreply.github.com>
2022-04-05 08:50:50 -0700

    Redo trimmomatic.bash using Dr. Cartwright's github post

--- scripts/trimmomatic.bash
+++ scripts/trimmomatic.bash
@@ -1,34 +1,29 @@
-#This is a wrapper for the trimmomatic tool on AGAVE
-#To load trimmomatic: module add trimmomatic/0.33
-#Example command: bash trimmomatic.bash PE SRR2589044_1.fastq.gz SRR2589044_2.fastq.gz SRR2589044_1_trim.fastq.gz SRR2589044_1_1un.fastq.gz SRR2589044_2_trim.fastq.g
-#z SRR2589044_2_1un.fastq.gz SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
-#TrimmomaticPE: Started with arguments: SRR2589044_1.fastq.gz SRR2589044_2.fastq.gz SRR2589044_1_trim.fastq.gz SRR2589044_1_1un.fastq.gz SRR2589044_2_
-#trim.fastq.gz SRR2589044_2_1un.fastq.gz SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
+#!/bin/bash
 
+# Wrapper Script for Trimmomatic
+#   USAGE: bash trimmomatic.bash PATH/TO/INPUT_1.fastq.gz PATH/TO/OUTPUT/DIR
 
-#Load module
+# Load Module
 module purge
 module add trimmomatic/0.33
 
+# Store script arguments to variables
 infile="$1"
 outdir="$2"
 
+# Create output directory if neccessary
 mkdir -p "$outdir"
 
-#Trim Jar
-TRIM_BIN=java -jar /packages/7x/trimmomatic/0.33/trimmomatic.jar 
-TRIM_ARGS="SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:scripts/NexteraPE-PE.fa:2:40:15" 
+# Trimmomatic's path and arguments
+TRIM_BIN="java -jar /packages/7x/trimmomatic/0.33/trimmomatic.jar"
+TRIM_ARGS="SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:scripts/NexteraPE-PE.fa:2:40:15"
 
-
-#This is a for loop that will allow the user to trim multiple sets of paired end reads
-
-#Create basename
+# Find the basename and input directory of our input file
 base=$(basename "${infile}" _1.fastq.gz)
 indir=$(dirname "${infile}")
 
-#Run Trimmomatic
+# Run Trimmomatic
 $TRIM_BIN PE "${indir}/${base}_1.fastq.gz" "${indir}/${base}_2.fastq.gz" \
-                "${indir}/${base}_1.trim.fastq.gz" "${indir}/${base}_1un.trim.fastq.gz" \
-                "${indir}/${base}_2.trim.fastq.gz" "${indir}/${base}_2un.trim.fastq.gz" \
-		${TRIM_ARGS}
- 
+  "${outdir}/${base}_1.trim.fastq.gz" "${outdir}/${base}_1un.trim.fastq.gz" \
+  "${outdir}/${base}_2.trim.fastq.gz" "${outdir}/${base}_2un.trim.fastq.gz" \
+  $TRIM_ARGS
