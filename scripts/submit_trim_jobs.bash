3a3239f4fc3d777ab5630b3c5cc48093a269730c
Emma Murari <emmurari@asu.edu>
2022-04-05 09:04:14 -0700

    Add a support for sbatch in our trimmomatic script

--- scripts/submit_trim_jobs.bash
+++ scripts/submit_trim_jobs.bash
@@ -1,8 +1,11 @@
+# Trimmomatic job submission script
+# Setup variables
 metadata=data/untrimmed_fastq/data_urls.txt
-outdir=data/trimed_fastq
-infiles=$(cut -d' ' -f 1 $metadata | grep _1.fastq.gz)
 indir=data/untrimmed_fastq
+infiles=$(cut -d' ' -f 1 $metadata | grep _1.fastq.gz)
+outdir=data/trimmed_fastq
 
-for filename in $infiles do
+# Submit one job per input file
+for filename in $infiles; do
   sbatch scripts/trimmomatic.bash $indir/$filename $outdir
 done
--- scripts/trimmomatic.bash
+++ scripts/trimmomatic.bash
@@ -3,6 +3,17 @@
 # Wrapper Script for Trimmomatic
 #   USAGE: bash trimmomatic.bash PATH/TO/INPUT_1.fastq.gz PATH/TO/OUTPUT/DIR
 
+#SBATCH -N 1  # number of nodes
+#SBATCH -n 1  # number of "tasks" (default: allocates 1 core per task)
+#SBATCH -t 0-00:10:00   # time in d-hh:mm:ss
+#SBATCH -p serial       # partition 
+#SBATCH -q normal       # QOS
+#SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
+#SBATCH --mail-user=YOUR-EMAIL-ADDRESS # Mail-to address
+#SBATCH --export=NONE   # Purge the job-submitting shell environment
+
 # Load Module
 module purge
 module add trimmomatic/0.33
